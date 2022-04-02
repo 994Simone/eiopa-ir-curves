a657794aa7aeeef4c8f11292255a558ed261e7b8
994Simone <50179380+994Simone@users.noreply.github.com>
2022-04-02 11:28:51 +0200

    Delete interest_rates directory
    Folder no longer needed. The function is inside eiopa.py script

--- interest_rates/forward_rates.py
+++
@@ -1,7 +0,0 @@
-# Function to calculate interest rates forward 1y
-
-def calc_1y_forward_rates(df,curve):
-    df[curve+"_shift"] = df[curve].shift(1, fill_value=0)
-    df[curve+"_forward"] = (((1 + df[curve]) ** df["Maturity"]) / 
-                        ((1 + df[curve+"_shift"]) ** (df["Maturity"] - 1)) - 1)
-    return df
